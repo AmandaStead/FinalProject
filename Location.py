2d8e2d22bc7b46e79dd78836e2677be05cbc1b53
Aarti Pandya <apandya1@adient.com>
2023-07-30 16:56:43 -0400

    Updated Location.py and LocationTitle.html with minor changes.

--- Location.py
+++ Location.py
@@ -4,11 +4,12 @@ from flask import Flask, request, jsonify, make_response, render_template
 app = Flask(__name__)
 
 
+# Routing to the local host with given "Location"
 @app.route("/Location")
 def booking():
     return render_template('LocationTitle.html')
 
 
-
+# Below code will run on local host with port 8000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
