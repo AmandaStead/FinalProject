d35bd6e30c0c70f90d7f9fd8d9a6cc54fe47f51e
Aarti Pandya <apandya1@adient.com>
2023-07-30 16:57:34 -0400

    Updated Booking.py with minor changes.

--- Booking.py
+++ Booking.py
@@ -3,11 +3,12 @@ from flask import Flask, request, jsonify, make_response, render_template
 
 app = Flask(__name__)
 
-
+# Routing to the local host with given "Location"
 @app.route("/Booking")
 def booking():
     return render_template('BookingTitles.html')
 
 
+# Below code will run on local host with port 8000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
